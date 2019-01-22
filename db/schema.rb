1d6ab4040d737ae94a70f658e2c9e1ce2d139adc
Roman Ustiantcev <liberrom.hse@gmail.com>
2019-01-22 13:39:56 +0100

    relation to answers - added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_21_154138) do
+ActiveRecord::Schema.define(version: 2019_01_22_121044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,10 @@ ActiveRecord::Schema.define(version: 2019_01_21_154138) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "answer_id"
+    t.index ["answer_id"], name: "index_questions_on_answer_id"
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "answers"
 end
