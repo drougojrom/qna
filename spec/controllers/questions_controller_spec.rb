551f4b60a8de581bcb6bb31f4a50854d5d2db06a
Roman Ustiantcev <liberrom.hse@gmail.com>
2019-01-26 18:34:38 +0100

    create + tests - added

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -52,4 +52,27 @@ RSpec.describe QuestionsController, type: :controller do
     end
   end
 
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new question into db' do
+        expect { post :create, params: { question: attributes_for(:question) }}.to change(Question, :count).by(1)
+      end
+      it 'redirects to show' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the question' do
+        expect { post :create, params: { question: attributes_for(:question, :invalid) }}.to_not change(Question, :count)
+      end
+
+      it 'renders new' do
+        post :create, params: { question: attributes_for(:question, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+
 end
