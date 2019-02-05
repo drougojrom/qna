f8611a52ee4e281bd5bef0020e5c7858f8358a96
Roman Ustiantcev <liberrom.hse@gmail.com>
2019-02-05 23:36:04 +0100

    remove spaces, test for author - added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
   it { should have_many(:questions) }
   it { should have_many(:answers) }
 
+  context 'is author' do
+    let!(:question) { create(:question) }
+
+    it 'user is the author of question' do
+      expect(question.user.is_author?(question)).to be(true)
+    end
+  end
 end
