467bebaab13749acb0b41222bf875ebb09378110
Roman Ustiantcev <liberrom.hse@gmail.com>
2019-02-05 16:29:45 +0100

    create answer for unauthenticated user - tests

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -8,23 +8,32 @@ feature 'User can create an answer for a question', %q{
   given(:user) { create(:user) }
   given(:question) { create(:question) }
 
-  background do
-    sign_in(user)
+  describe 'An authenticated user answers on the question' do
+    background do
+      sign_in(user)
+      visit question_path(question)
+      click_on 'Answer'
+    end
+
+    scenario 'answer on a question with errors' do
+      click_on 'Answer'
+
+      expect(page).to have_content "Body can't be blank"
+    end
+
+    scenario 'answer on a question' do
+      fill_in 'Body', with: 'test answer'
+      click_on 'Answer'
+
+      expect(page).to have_content 'Your answer successfully created'
+      expect(page).to have_content 'test answer'
+    end
+  end
+
+  scenario 'Unauthenticated user tries to answer on a question' do
     visit question_path(question)
     click_on 'Answer'
-  end
 
-  scenario 'answer on a question with errors' do
-    click_on 'Answer'
-
-    expect(page).to have_content "Body can't be blank"
-  end
-
-  scenario 'answer on a question' do
-    fill_in 'Body', with: 'test answer'
-    click_on 'Answer'
-
-    expect(page).to have_content 'Your answer successfully created'
-    expect(page).to have_content 'test answer'
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 end
