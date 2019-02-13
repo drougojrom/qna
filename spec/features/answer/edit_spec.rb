d240c58296284c4b2d7e67ec8b92872ce76a18fb
Roman Ustiantcev <liberrom.hse@gmail.com>
2019-02-13 16:16:12 +0100

    edit answer, tests - added

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -1,19 +1,16 @@
 require 'rails_helper'
 
-feature 'User can edit his answer', %q{
+feature 'User can edit his question', %q{
   In order to correct the mistakes
   an author of answer should be able to 
-  edit his answer.
+  edit his question
 } do
-
   given(:user) { create(:user) }
   given(:question) { create(:question) }
   given(:answer) { create :answer, question: question, user: user }
 
   scenario 'Unauthenticated user can not edit an answer' do
     visit question_path(answer.question)      
-
-
     expect(page).to_not have_link 'Edit'
   end
 
@@ -34,10 +31,22 @@ feature 'User can edit his answer', %q{
       end
     end
     scenario 'edits his answer with errors' do
+      sign_in user
+      visit question_path answer.question
 
+      click_on 'Edit'
+      within '.answers' do
+        fill_in 'Your answer', with: ''
+        click_on 'Update'
+
+        expect(page).to have_content answer.body
+        expect(page).to_not have_content ''
+        expect(page).to have_selector 'textarea'
+      end
     end
     scenario 'tries to edit other users answer' do
-
+      visit question_path(answer.question)      
+      expect(page).to_not have_link 'Edit'
     end
   end
 end
