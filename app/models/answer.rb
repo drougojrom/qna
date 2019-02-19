bee14e47ce129a7b8396c1dc5c7c0ae739376294
Roman Ustiantcev <liberrom.hse@gmail.com>
2019-02-19 11:08:57 +0100

    post-review minor fixes

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,7 +5,7 @@ class Answer < ApplicationRecord
   validates :body, presence: true
 
   default_scope { order("right_answer DESC").order("created_at DESC") }
-  scope :correct_answers, -> { where("right_answer = ?", true) }
+  scope :correct_answers, -> { where(right_answer: true) }
 
   def make_right_answer(user, correct)
     correct ? make_correct(user) : make_not_correct(user)
@@ -14,10 +14,10 @@ class Answer < ApplicationRecord
   private
 
   def make_correct(user)
-    transaction do
-      if user.author_of?(self.question)
-        self.question.answers.correct_answers.update_all("right_answer = false")
-        self.reload.update!(right_answer: true)
+    if user.author_of?(question)
+      transaction do
+        question.answers.correct_answers.update_all("right_answer = false")
+        reload.update!(right_answer: true)
       end
     end
   end
