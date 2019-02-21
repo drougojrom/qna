55e3fa5c7581fca9e18fb5c3330e72bbddf70bfb
Roman Ustiantcev <liberrom.hse@gmail.com>
2019-02-21 14:22:51 +0100

    attachment controller delete fixes

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -2,14 +2,8 @@ class AttachmentsController < ApplicationController
   before_action :authenticate_user!
 
   def destroy
-    @file = ActiveStorage::Attachment.find_by(id: params[:id])
+    @file = ActiveStorage::Attachment.find(params[:id])
     @file.purge
     render :destroy
-  end
-
-  private 
-
-  def attachment_params
-    params.require(:attachment).permit(:id)
   end
 end
--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe AttachmentsController, type: :controller do
+  describe 'DELETE #destroy' do
+    let!(:question_with_attached_file) { create :question_with_attached_file }
 
+    context 'user is the author of question' do
+      before { log_in question_with_attached_file.user }
+
+      it 'deletes the attachment' do
+        expect { delete :destroy, params: { id: question_with_attached_file.files.first.id }, format: :js }.to change(ActiveStorage::Attachment, :count).by(-1)
+        expect(response).to render_template :destroy
+      end
+    end
+    context 'user is not the author of question' do
+      let!(:new_user) { create(:user) }
+      before { log_in new_user }
+
+      it 'does not delete the question' do
+        expect { delete :destroy, params: { id: question_with_attached_file.files.first.id }, format: :js }.not_to change(ActiveStorage::Attachment, :count)
+        expect(response).to redirect_to question
+      end
+    end
+  end
 end
