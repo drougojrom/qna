bf7880428e549dbff1565ae2e38f9894c4011928
Roman Ustiantcev <liberrom.hse@gmail.com>
2019-03-04 15:11:53 +0100

    tests for unauthenticated user and user non-author - added

--- spec/features/question/remove_links_spec.rb
+++ spec/features/question/remove_links_spec.rb
@@ -6,6 +6,7 @@ feature 'User can remove links from his question', %q{
   links from my question
 } do
   given(:question) { create :question, :with_link }
+  given(:user) { create(:user) }
 
   scenario 'User adds links when asks a question', js: true do
     sign_in question.user
@@ -17,6 +18,23 @@ feature 'User can remove links from his question', %q{
       expect(page).to have_link 'Delete link'
       click_on 'Delete link'
       expect(page).to_not have_link 'MyString'
+      expect(page).to_not have_link 'Delete link'
+    end
+  end
+
+  scenario 'user is not the author of the question' do
+    sign_in(user)
+    visit question_path(question)
+
+    within '.links' do
+      expect(page).not_to have_link 'Delete'
+    end
+  end
+
+  scenario 'Unauthenticated user tries to delete a question' do
+    visit question_path(question)
+    within '.links' do
+      expect(page).to_not have_link 'Delete link'
     end
   end
 end
