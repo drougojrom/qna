ad89114379c8eac7b6b9ad0b6fec5404c2777c34
Roman Ustiantcev <liberrom.hse@gmail.com>
2019-03-18 11:06:44 +0100

    votes test for and against - added

--- spec/controllers/shared/voting_example.rb
+++ spec/controllers/shared/voting_example.rb
@@ -6,19 +6,99 @@ shared_examples 'voting' do
   let(:votable) { create votable_class }
 
   describe 'POST #revoke' do
-    context 'authorized user' do
-      context 'on existing' do
-        context 'positive vote' do
-          let!(:vote) { create :vote }
-          before { sign_in vote.user }
-          it 'decreases rating' do
-            votable = vote.votable
-            post :vote_revoke, params: {id: votable.id}, format: :json
-            expect { votable.reload }.to change(votable, :rating).by(-1)
-          end
-        end
+    context 'unauthorized user' do
+      let!(:vote) { create :vote, votable: votable, value: 1 }
+      it 'response 401 unauthorized' do
+        post :vote_revoke, params: {id: votable.id}, format: :json
+        expect(response).to have_http_status(401)
+      end
+
+      it 'not change votes count in db' do
+        expect { post :vote_revoke, params: {id: votable.id} , format: :json }.to_not change(Vote, :count)
+      end
+
+      it 'not increases votable rating' do
+        post :vote_revoke, params: {id: votable.id}, format: :json
+        expect { votable.reload }.to_not change(votable, :rating)
+      end
+    end
+
+    context 'authorized user on existing vote' do
+      let!(:vote) { create :vote, votable: votable, value: 1 }
+      before { sign_in vote.user }
+
+      it 'decreases rating' do
+        post :vote_revoke, params: {id: votable.id}, format: :json
+        expect { votable.reload }.to change(votable, :rating).by(-1)
+      end
+
+      it 'delete vote from db' do
+        votable = vote.votable
+        expect { post :vote_revoke, params: { id: votable.id }, format: :json }.to change(Vote, :count).by(-1)
       end
     end
   end
 
+  describe 'POST #vote_for' do
+    context 'unauthorized user' do
+      let!(:vote) { create :vote, votable: votable, value: 1 }
+      it 'response 401 unauthorized' do
+        post :vote_for, params: {id: votable.id}, format: :json
+        expect(response).to have_http_status(401)
+      end
+
+      it 'not change votes count in db' do
+        expect { post :vote_for, params: {id: votable.id} , format: :json }.to_not change(Vote, :count)
+      end
+
+      it 'increases votable rating' do
+        post :vote_for, params: {id: votable.id}, format: :json
+        expect { votable.reload }.to_not change(votable, :rating)
+      end
+    end
+
+    context 'authorized user votes' do
+      before { sign_in votable.user }
+
+      it 'increases rating' do
+        post :vote_for, params: {id: votable.id}, format: :json
+        expect { votable.reload }.to change(votable, :rating).by 1
+      end
+
+      it 'add vote to db' do
+        expect { post :vote_for, params: { id: votable.id }, format: :json }.to change(Vote, :count).by(1)
+      end
+    end
+  end
+
+  describe 'POST #vote_against' do
+    context 'unauthorized user' do
+      it 'response 401 unauthorized' do
+        post :vote_against, params: {id: votable.id}, format: :json
+        expect(response).to have_http_status(401)
+      end
+
+      it 'not change votes count in db' do
+        expect { post :vote_against, params: {id: votable.id} , format: :json }.to_not change(Vote, :count)
+      end
+
+      it 'not increases votable rating' do
+        post :vote_against, params: {id: votable.id}, format: :json
+        expect { votable.reload }.to_not change(votable, :rating)
+      end
+    end
+
+    context 'authorized user votes' do
+      before { sign_in votable.user }
+
+      it 'decreases rating' do
+        post :vote_against, params: {id: votable.id}, format: :json
+        expect { votable.reload }.to change(votable, :rating).by(-1)
+      end
+
+      it 'adds vote to db' do
+        expect { post :vote_against, params: { id: votable.id }, format: :json }.to change(Vote, :count).by(1)
+      end
+    end
+  end
 end
