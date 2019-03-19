f4a608eb014dcf6ed60dbd0c697cfd1693f6e06e
Roman Ustiantcev <liberrom.hse@gmail.com>
2019-03-19 15:53:40 +0100

    helper fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,24 @@
 module ApplicationHelper
-  def vote_for_btn_state votable
-    unless signed_in? && current_user != votable.user && votable.user_vote(current_user).new_record?
+  def vote_for_btn_state(votable)
+    if signed_in? && current_user != votable.user && votable.user_vote(current_user).new_record?
+      'enabled'
+    else
       'disabled'
     end
   end
 
-  def vote_against_btn_state votable
-    unless signed_in? && current_user != votable.user && votable.user_vote(current_user).new_record?
+  def vote_against_btn_state(votable)
+    if signed_in? && current_user != votable.user && votable.user_vote(current_user).new_record?
+      'enabled'
+    else
       'disabled'
     end
   end
 
-  def vote_revoke_btn_state votable
-    unless signed_in? && current_user != votable.user && votable.user_vote(current_user).persisted?
+  def vote_revoke_btn_state(votable)
+    if signed_in? && current_user != votable.user && votable.user_vote(current_user).persisted?
+      'enabled'
+    else
       'disabled'
     end
   end
