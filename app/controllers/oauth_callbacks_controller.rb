53dff6bc33fb21798b9f48bbb7bcc1c4a5c9a805
Roman Ustiantcev <liberrom.hse@gmail.com>
2019-04-09 10:24:22 +0200

    small oauth refactoring

--- app/controllers/oauth_callbacks_controller.rb
+++ app/controllers/oauth_callbacks_controller.rb
@@ -2,8 +2,7 @@ class OauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.find_for_oauth(request.env['omniauth.auth'])
     if @user&.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
+      sign_in_user(@user, 'Github')
     else
       redirect_to root_path, alert: 'Something went wrong'
     end
@@ -12,8 +11,7 @@ class OauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
     @user = User.find_for_oauth(request.env['omniauth.auth'])
     if @user&.persisted? && @user.email_verified?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Twitter') if is_navigational_format?
+      sign_in_user(@user, 'Twitter')
     else
       finish_signup_for(@user)
     end
@@ -21,5 +19,12 @@ class OauthCallbacksController < Devise::OmniauthCallbacksController
 
   def finish_signup_for(resource)
     redirect_to finish_signup_path(resource)
+  end
+
+  private 
+
+  def sign_in_user(user, provider)
+    sign_in_and_redirect user, event: :authentication
+    set_flash_message(:notice, :success, kind: provider) if is_navigational_format?
   end
 end
