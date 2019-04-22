5a4860abdb424c248f00b784c3af1825b2b30f95
Roman Ustiantcev <liberrom.hse@gmail.com>
2019-04-22 15:02:51 +0300

    api call for get all_users - added

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -5,4 +5,8 @@ class Api::V1::ProfilesController < Api::V1::BaseController
   def me
     render json: current_resource_owner
   end
+
+  def all_users
+    render json: User.all_except(current_resource_owner)
+  end
 end
